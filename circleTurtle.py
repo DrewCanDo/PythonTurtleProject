ab2b983379e00f963c1901d5f6b7b617fefaf34e
Andrew Brown <devandrewbro@gmail.com>
2024-12-04 18:01:14 -0500

    made a triangle

--- circleTurtle.py
+++ circleTurtle.py
@@ -9,10 +9,40 @@ circle(15)
 """
 
 # color('red')
-color('#ffd000') #gold color
+""" color('#ffd000') #gold color
 forward(100)
 color('#00ff48') #green color
 right(90)
+forward(100) """
+
+# challenge color circles
+""" color('green')
+circle(100)
+color('red')
+circle(50)
+color('purple')
+circle(25) """
+
+""" color('red')
+begin_fill()
+circle(50)
+end_fill() """
+
+# challenge 
+""" As a bit of a challenge, draw and fill in an equilateral triangle.
+
+An equilateral triangle, is a triangle where all sides are of equal length,
+thus all of the angles must be the same too. For this challenge, make each
+side of the triangle 100 pixels. """
+
+color('blue')
+begin_fill()
 forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+left(120)
+end_fill()
 
 done() 
