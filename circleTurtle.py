437740079a0d0f30e59fbe761c871a4bfb5feed2
Andrew Brown <devandrewbro@gmail.com>
2024-12-04 17:45:44 -0500

    Update lesson for color

--- circleTurtle.py
+++ circleTurtle.py
@@ -1,5 +1,18 @@
 from turtle import *
 
+"""
 circle(50)
+forward(100)
+circle(50)
+circle(30)
+circle(15)
+"""
+
+# color('red')
+color('#ffd000') #gold color
+forward(100)
+color('#00ff48') #green color
+right(90)
+forward(100)
 
 done() 
