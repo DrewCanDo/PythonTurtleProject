edef70bae73da18be534d68e35cfa312c3896938
Andrew Brown <devandrewbro@gmail.com>
2024-12-04 18:02:59 -0500

    background color swap

--- circleTurtle.py
+++ circleTurtle.py
@@ -35,14 +35,17 @@ An equilateral triangle, is a triangle where all sides are of equal length,
 thus all of the angles must be the same too. For this challenge, make each
 side of the triangle 100 pixels. """
 
-color('blue')
-begin_fill()
-forward(100)
-left(120)
-forward(100)
-left(120)
-forward(100)
-left(120)
-end_fill()
+# color('blue')
+# begin_fill()
+# forward(100)
+# left(120)
+# forward(100)
+# left(120)
+# forward(100)
+# left(120)
+# end_fill()
+
+# how to change background color
+bgcolor('yellow')
 
 done() 
