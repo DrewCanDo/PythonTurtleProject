cc9f765c231cb13d6bb47c48d099cdba47836021
Andrew Brown <devandrewbro@gmail.com>
2024-12-11 18:43:28 -0500

    BallononPopper Project

--- ballonPopperProject/ballonPopper.py
+++ ballonPopperProject/ballonPopper.py
@@ -1,1 +1,33 @@
 #start
+
+#Definition alt+shift+A multi line comments
+""" 1. be able to detect inputs.
+2. require multiple inputs to pop the balloon.
+3. Utilize variables, conditions and functions. """
+
+#import turtle
+from turtle import *
+
+diameter = 40
+pop_diameter = 100
+
+def draw_balloon ():
+    color("red")
+    dot(diameter)
+
+def inflate_balloon ():
+    global diameter
+    diameter = diameter + 10
+    draw_balloon()
+
+    if diameter >= pop_diameter:
+        clear()
+        diameter = 40
+        write ("POP!")
+
+draw_balloon()
+
+onkey(inflate_balloon, "Up")
+listen()
+
+done()
